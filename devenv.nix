6dbf25256b27a8d9d36a7e18b3f94a7073989979
Nathan Marien <yohanpro.jimdo.com@gmail.com>
2025-04-14 10:31:30 -0400

    devenv

---
+++ devenv.nix
@@ -0,0 +1,55 @@
+{ pkgs, lib, config, inputs, ... }:
+
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+	android = {
+		enable = true;
+		flutter = {
+			enable = true;
+			package = pkgs-unstable.flutter;
+		};
+		android-studio = {
+      enable = true;
+      package = pkgs.android-studio;
+    };
+	};
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  enterShell = ''
+		adb start-server &
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
